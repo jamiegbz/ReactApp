f03d8ea2fcc136f253172c20e297293f060828ad
Jamie  Gomez <jamiegomez@MacBook-Air.local>
2024-11-27 01:34:22 -0500

    expanded sidebar

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -3,14 +3,34 @@
 //updated
 ///toolbar
 
+import { useState } from "react"
 
 
 
 export default function ItemCard(){
+    //USED state to expand sidebar
+    const [isExpanded, setIsExpanded] = useState(true)
+
+    const handleButtonClick = () => {
+        setIsExpanded(!isExpanded)
+    }
+
     return (
-    <div className=" bg-light p-3 border-bottom">
-        sidebar
-    
-    </div>
+        <>
+            {isExpanded ? (
+                <div
+                    className=" bg-light p-3 border-bottom"
+                    >
+                    SIDE BAR
+                </div>
+                
+            ) : null }
+            <button 
+                className="btn btn-light p-1 border" 
+                onClick ={handleButtonClick}
+                > 
+                { isExpanded ? "<" : ">" }
+            </button>
+        </>
     )
 }
